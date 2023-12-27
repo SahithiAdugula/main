9bff824ed37e641ad0315d0db805b359d917f4dd
SahithiAdugula <154987306+SahithiAdugula@users.noreply.github.com>
2023-12-27 19:01:42 +0530

    Add files via upload
    First Commit
    Uploaded All Files

---
+++ Hospital_Management/src/util/DBConnUtil.java
@@ -0,0 +1,36 @@
+package util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnUtil {
+	
+	public static Connection getConnection(String props)
+	{
+		Connection connection=null;
+		String[] properties=props.split(" ");
+		//System.out.println(properties);
+		try {
+			Class.forName(properties[2]);
+			System.out.println("class loaded");
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			System.out.println("Class Not Established");
+			e.printStackTrace();
+		}
+		
+		try {
+			connection = DriverManager.getConnection(  
+					properties[3],properties[0],properties[1]);
+			System.out.println("con established");
+		} catch (SQLException e) {
+			
+			
+			e.printStackTrace();
+		}
+		
+		return connection;
+	}
+
+}
---
+++ Hospital_Management/src/util/DBPropertyUtil.java
@@ -0,0 +1,30 @@
+package util;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+public class DBPropertyUtil {
+	public static String getPropertyString(String file_name)
+	{
+		FileInputStream fis = null;
+		try {
+			fis = new FileInputStream(file_name);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+			System.out.println(file_name+" File is not found");
+		}
+		Properties p1=new Properties();
+		try {
+			p1.load(fis);
+		} catch (IOException e) {
+			
+			e.printStackTrace();
+		}
+		String propstr=p1.getProperty("username")+" "+p1.getProperty("password")+" "+p1.getProperty("driver")+" "+p1.getProperty("url");
+		return propstr;
+		
+	}
+
+}
